e9a0ad7c09a855ab087cd6cdbfa61e51ce328749
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-12-17 17:56:04 +0100

    Added theme provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+
 import "./index.css";
 import CustomRoutes from "./routes/routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CustomRoutes />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <CustomRoutes />
+    </ThemeProvider>
   </StrictMode>
 );

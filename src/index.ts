febd701f33457f49bf786be9da6bf02079c10816
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-12-17 16:24:04 +0100

    Added web socket config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
 import { Hono } from "hono";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { env } from "./utils/config";
 
 const app = new Hono();
 
+const httpServer = createServer();
+const io = new Server(httpServer);
+
+app.get("/api/test", (c) => {
+  return c.json({ message: "test" });
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+
+  socket.on("message", (data) => {
+    console.log("Received message:", data);
+    socket.emit("chat response", { text: `Server received: ${data.text}` });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnect", socket.id);
+  });
+});
+
+httpServer.listen(env.CHAT_PORT, () => {
+  console.log(`Chat server running on PORT: ${env.CHAT_PORT}`);
+});
+
 const server = Bun.serve({
-  port: process.env.PORT,
+  port: env.PORT,
   fetch: app.fetch,
 });
 
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,9 +7,15 @@ const envSchema = z.object({
     .transform((val) => parseInt(val, 10))
     .refine((val) => !isNaN(val), { message: "PORT must be a number" })
     .default("3000"),
+  CHAT_PORT: z
+    .string()
+    .transform((val) => parseInt(val, 10))
+    .refine((val) => !isNaN(val), { message: "PORT must be a number" })
+    .default("4000"),
 });
 
 export const env = envSchema.parse({
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
+  CHAT_PORT: process.env.CHAT_PORT,
 });

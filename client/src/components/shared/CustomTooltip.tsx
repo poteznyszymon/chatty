0abaa639368d87ea17e022ec4ad7c295799131e7
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2024-12-28 21:43:19 +0100

    Refactor CustomTooltip to use Tooltip components for enhanced functionality

--- client/src/components/shared/CustomTooltip.tsx
+++ client/src/components/shared/CustomTooltip.tsx
@@ -1,5 +1,27 @@
-const CustomTooltip = () => {
-  return <div>CustomTooltip</div>;
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+
+interface CustomTooltipProps {
+  text: string;
+  children: React.ReactNode;
+  classname?: string;
+}
+
+const CustomTooltip = ({ text, children, classname }: CustomTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent className={classname}>
+          <p>{text}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
 };
 
 export default CustomTooltip;

c677191c477067d6a4f0f8e31f6a437ad7c10328
poteznyszymon <77408864+poteznyszymon@users.noreply.github.com>
2025-01-27 20:26:47 +0100

    Add Babel plugins for React component optimization and update Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,15 @@
 import path from "path";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-component"]],
+      },
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
